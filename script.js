f26e6ff9ee521e41d3ea45722c20c352eaa729b9
Anurag Ranjan <anuragranjan125@gmail.com>
2022-11-15 00:48:41 +0530

    Updated EventListener function

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ let prevActive;
 
 allPanel.forEach(function (e) {
   e.addEventListener("click", function exec(e) {
-    console.log("k");
     if (prevActive === this) {
       prevActive.classList.toggle("active");
       console.log("If Block");
     } else {
+      prevActive && prevActive.classList.remove("active");
       this.classList.toggle("active");
       prevActive = this;
       console.log("Else Block");
     }
+
+    // removeActive();
+    // this.cla
   });
 });
